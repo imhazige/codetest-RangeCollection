c218da4072afec904811955b242ded820e884550
k.z <>
2019-03-07 17:25:12 +0800

    set up project 2

--- common/range-collection.js
+++ common/range-collection.js
@@ -7,69 +7,81 @@
  * NOTE: Feel free to add any extra member variables/functions you like.
  */
 class RangeCollection {
-    /**
-     * Adds a range to the collection
-     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-     */
-    add(range) {
-      // TODO: implement this
+  validateRange() {}
+
+  /**
+   * Adds a range to the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  add(range) {
+    // TODO: implement this
+    if (!range || !range.length) {
+      return;
     }
-  
-    /**
-     * Removes a range from the collection
-     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-     */
-    remove(range) {
-      // TODO: implement this
+    const [start, end] = range;
+    if (start > range) {
+      throw new Error(`invalid range`);
     }
-  
-    /**
-     * Prints out the list of ranges in the range collection
-     */
-    print() {
-      // TODO: implement this
+    if (start == end) {
+      return;
     }
   }
-  
-  // Example run
-  const rc = new RangeCollection();
-  
-  rc.add([1, 5]);
-  rc.print();
-  // Should display: [1, 5)
-  
-  rc.add([10, 20]);
-  rc.print();
-  // Should display: [1, 5) [10, 20)
-  
-  rc.add([20, 20]);
-  rc.print();
-  // Should display: [1, 5) [10, 20)
-  
-  rc.add([20, 21]);
-  rc.print();
-  // Should display: [1, 5) [10, 21)
-  
-  rc.add([2, 4]);
-  rc.print();
-  // Should display: [1, 5) [10, 21)
-  
-  rc.add([3, 8]);
-  rc.print();
-  // Should display: [1, 8) [10, 21)
-  
-  rc.remove([10, 10]);
-  rc.print();
-  // Should display: [1, 8) [10, 21)
-  
-  rc.remove([10, 11]);
-  rc.print();
-  // Should display: [1, 8) [11, 21)
-  
-  rc.remove([15, 17]);
-  rc.print();
-  // Should display: [1, 8) [11, 15) [17, 21)
-  
-  rc.remove([3, 19]);
-  rc.print();
-  // Should display: [1, 3) [19, 21)
+
+  /**
+   * Removes a range from the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  remove(range) {
+    // TODO: implement this
+  }
+
+  /**
+   * Prints out the list of ranges in the range collection
+   */
+  print() {
+    // TODO: implement this
+  }
+}
+
+// Example run
+const rc = new RangeCollection();
+
+rc.add([1, 5]);
+rc.print();
+// Should display: [1, 5)
+
+rc.add([10, 20]);
+rc.print();
+// Should display: [1, 5) [10, 20)
+
+rc.add([20, 20]);
+rc.print();
+// Should display: [1, 5) [10, 20)
+
+rc.add([20, 21]);
+rc.print();
+// Should display: [1, 5) [10, 21)
+
+rc.add([2, 4]);
+rc.print();
+// Should display: [1, 5) [10, 21)
+
+rc.add([3, 8]);
+rc.print();
+// Should display: [1, 8) [10, 21)
+
+rc.remove([10, 10]);
+rc.print();
+// Should display: [1, 8) [10, 21)
+
+rc.remove([10, 11]);
+rc.print();
+// Should display: [1, 8) [11, 21)
+
+rc.remove([15, 17]);
+rc.print();
+// Should display: [1, 8) [11, 15) [17, 21)
+
+rc.remove([3, 19]);
+rc.print();
+// Should display: [1, 3) [19, 21)
